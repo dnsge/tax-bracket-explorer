a18175027a253fecc9d9fcd075c24ffacc00689e
Daniel Sage <24928223+dnsge@users.noreply.github.com>
2024-12-05 20:20:22 -0500

    add 2025 us federal rates

--- src/TaxPresets.ts
+++ src/TaxPresets.ts
@@ -39,6 +39,35 @@ export const TAX_PRESETS: TaxPresetConfig[] = [
     ],
   },
   {
+    id: "us-federal-2025",
+    name: "US Federal 2025",
+    description: "US Federal Income Tax Brackets for 2025 (Single Filer)",
+    brackets: [
+      { min: 0, rate: 10 },
+      { min: 11925, rate: 12 },
+      { min: 48475, rate: 22 },
+      { min: 103350, rate: 24 },
+      { min: 197300, rate: 32 },
+      { min: 250525, rate: 35 },
+      { min: 626350, rate: 37 },
+    ],
+  },
+  {
+    id: "us-federal-2025-married",
+    name: "US Federal 2025 (Married)",
+    description:
+      "US Federal Income Tax Brackets for 2025 (Married Filing Jointly)",
+    brackets: [
+      { min: 0, rate: 10 },
+      { min: 23850, rate: 12 },
+      { min: 96950, rate: 22 },
+      { min: 206700, rate: 24 },
+      { min: 394600, rate: 32 },
+      { min: 501050, rate: 35 },
+      { min: 751600, rate: 37 },
+    ],
+  },
+  {
     id: "custom",
     name: "Custom",
     description: "Custom tax brackets",
